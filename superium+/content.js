35fa78b7ac1a87856bd31634230a301972ac37fb
Beriff <dofite.yes@gmail.com>
2020-10-08 14:52:23 +0700

    Update content.js

--- superium+/content.js
+++ superium+/content.js
@@ -1,89 +1,339 @@
+//only god can judge me for the following shit i wrote
+
+
+
 chrome.storage.sync.get(["theme"], function(items){
     //  items = [ { "yourBody": "myBody" } ]
 
     console.log(items['theme'])
+    let outerSelector
+
+    function changeBg(class_, col='black') {
+        if (class_.includes('.') || class_.includes('#')) {
+            outerSelector = document.querySelectorAll(class_);
+            for (let i = 0; i < outerSelector.length; i++) {
+                outerSelector[i].style.backgroundColor = col;
+            }
+        } else {
+            outerSelector = document.getElementsByTagName(class_)[0]
+            outerSelector.style.backgroundColor = col;
+        }
+
+    }
+    function changeBgCol(class_, col='black', textcol='green', flag=false) {
+
+        if ( (class_.includes('.') || class_.includes('#')) || flag) {
+            outerSelector = document.querySelectorAll(class_);
+            for (let i = 0; i < outerSelector.length; i++) {
+                outerSelector[i].style.backgroundColor = col;
+                outerSelector[i].style.color = textcol;
+            }
+        } else {
+            outerSelector = document.getElementsByTagName(class_)[0]
+            outerSelector.style.backgroundColor = col;
+            outerSelector.style.color = textcol;
+        }
+
+    }
+    function changeCol(class_, col='#eee') {
+        outerSelector = document.querySelectorAll(class_);
+            for (let i = 0; i < outerSelector.length; i++) {
+                outerSelector[i].style.color = col;
+            }
+    }
+
+    function changeBgBorder(class_, col='rgba(0, 0, 0, 0)', border='1px solid #48c774') {
+
+        outerSelector = document.querySelectorAll(class_);
+        for (let i = 0; i < outerSelector.length; i++) {
+            outerSelector[i].style.backgroundColor = col //+ ' !important';
+            outerSelector[i].style.border = border //+ ' !important';
+        }
+
+    }
+
 
     if (items['theme'] == "PureBlack") {
 
-        console.log(items['theme'])
-
         let navbar = document.getElementById('spNavbar')
         navbar.style.backgroundColor = 'black'
 
         document.body.style.backgroundColor = 'black'
-        let myElements = document.querySelectorAll(".card");
+        
+        changeBg('.card')
+        changeBg('.panel-block')
+        changeBg('.box')
+        changeBg('.navbar-brand')
+        changeBg('.container')
+        changeBg('.footer')
 
-        for (let i = 0; i < myElements.length; i++) {
-            myElements[i].style.backgroundColor = 'black';
-        }
-
-        let myElements2 = document.querySelectorAll(".panel-block");
-
-        for (let i = 0; i < myElements2.length; i++) {
-            myElements2[i].style.backgroundColor = 'black';
-        }
-
-        let myElements3 = document.querySelectorAll(".box");
-
-        for (let i = 0; i < myElements3.length; i++) {
-            myElements3[i].style.backgroundColor = 'black';
-        }
-
-        let myElements4 = document.querySelectorAll(".navbar-brand");
-
-        for (let i = 0; i < myElements4.length; i++) {
-            myElements4[i].style.backgroundColor = 'black';
-        }
-
-        let myElements5 = document.querySelectorAll(".container");
-
-        for (let i = 0; i < myElements5.length; i++) {
-            myElements5[i].style.backgroundColor = 'black';
-        }
+        
 
     }
-    else if (items['theme'] == 'Matrix') {
-        let navbar = document.getElementById('spNavbar')
-        navbar.style.backgroundColor = 'black'
+    else if (items['theme'] == 'Outlined') {
 
-        document.body.style.backgroundColor = 'black'
-        document.body.style.color = "green"
-        let myElements = document.querySelectorAll(".card");
-
-        for (let i = 0; i < myElements.length; i++) {
-            myElements[i].style.backgroundColor = 'black';
-            myElements[i].style.color = 'green';
-        }
-
-        let myElements2 = document.querySelectorAll(".panel-block");
-
-        for (let i = 0; i < myElements2.length; i++) {
-            myElements2[i].style.backgroundColor = 'black';
-            myElements2[i].style.color = 'green'
-        }
-
-        let myElements3 = document.querySelectorAll(".box");
-
-        for (let i = 0; i < myElements3.length; i++) {
-            myElements3[i].style.backgroundColor = 'black';
-            myElements3[i].style.color = 'green'
-        }
-
-        let myElements4 = document.querySelectorAll(".navbar-brand");
-
-        for (let i = 0; i < myElements4.length; i++) {
-            myElements4[i].style.backgroundColor = 'black';
-            myElements4[i].style.color = 'green'
-        }
-
-        let myElements5 = document.querySelectorAll(".container");
-
-        for (let i = 0; i < myElements5.length; i++) {
-            myElements5[i].style.backgroundColor = 'black';
-            myElements5[i].style.color = 'green'
-        }
+        changeBgBorder('.button.is-success')
+        changeBgBorder('.button.is-link', 'rgba(0, 0, 0, 0)', '2px solid #3273dc')
+        changeBgBorder('.button.is-small', 'rgba(0, 0, 0, 0)', '2px solid #3273dc')
+        changeBgBorder('.button.is-danger', 'rgba(0, 0, 0, 0)', '2px solid #e74940')
+        changeBgBorder('.ribbon.is-success', 'rgba(0, 0, 0, 0)', '1px solid #48c774')
+        changeBgBorder('.ribbon.is-danger', 'rgba(0, 0, 0, 0)', '1px solid #e74940')
     }
-});
+    else if (items['theme'] == "Flawless") {
+        /*changeBgCol('body', '#16171d', '#eee')
+        let placeholder = document.getElementById('spNavbar')
+        placeholder.style.backgroundColor = '#2b2d38'
+        placeholder.style.border = 'none'
+        placeholder.style.boxShadow = 'none'
+        changeCol('.navbar-burger')
+        changeBg('.navbar-menu.is-active', '#20222f')
+        for(let i in ['.navbar-link.is-active', '.navbar-link:focus', '.navbar-link:focus-within']) {
+            changeBgCol(i, 'rgba(0, 0, 0, 0.1)', '#fff')
+        }
+
+        changeBgCol*/
+
+        var css = `
+
+        body, html {
+            background-color:#16171d;
+            color:#eee;
+        }
+        #spNavbarLogo {
+            content:url("/images/navbar-logo-white.png");
+        }
+        #spNavbar {
+            background-color:#2b2d38;
+            border:none;
+            box-shadow:none;
+        }
+        .navbar-burger {
+            color:#eee!important;
+        }
+        .navbar-menu.is-active {
+            background-color:#20222f;
+        }
+        .navbar-link.is-active, .navbar-link:focus, .navbar-link:focus-within, .navbar-link:hover, a.navbar-item.is-active, a.navbar-item:focus, a.navbar-item:focus-within, a.navbar-item:hover {
+            background-color: rgba(0, 0, 0, 0.1);
+            color:#fff;
+        }
+        #spNavExtra .navbar-dropdown {
+            background-color:#2c2e3c;
+        }
+        #spNavExtra .navbar-dropdown .dropdown-divider {
+            background-color:#232735!important;
+        }
+        #sploggedindropdown .navbar-dropdown {
+            background-color:#2c2e3c;
+            width:12.4em;
+        }
+        .tooltiptext {
+            background-color:#484e6f!important;
+        }
+        #sploggedindropdown .navbar-dropdown .navbar-item .icon {
+            position:relative;
+            color:#fff;
+            right:7px;
+            opacity: 0;
+            margin-right:-30px;
+            transition:all .2s ease-in-out;
+        }
+        #sploggedindropdown .navbar-dropdown .navbar-item:hover .icon {
+            color:#669af7;
+            opacity: 1;
+            margin-right:0;
+            right:0;
+        }
+        #sploggedindropdown .navbar-dropdown .navbar-item.has-text-danger .icon {
+            position:relative;
+            top:1px;
+            right:7px;
+            opacity: 0;
+        }
+        #sploggedindropdown .navbar-dropdown .navbar-item.has-text-danger:hover .icon {
+            color:#f36d6d;
+            opacity: 1;
+            right:0;
+        }
+        .box {
+            background-color:#20212b;
+        }
+        .panel-heading {
+            background-color:#282a36;
+            color:#ccc;
+        }
+        .panel .panel-block {
+            background-color:#2c2f3e!important;
+            border-color:rgb(22 23 29 / 19%)!important;
+        }
+        .panel-block.is-active .panel-icon {
+            color:#2dd3b4;
+        }
+        .spmForumPost {
+            border-color:rgb(22 23 29 / 19%)!important;
+        }
+        .ribbon {
+            border-bottom-left-radius: 4px;
+            border-top-left-radius: 4px;
+        }
+        .card {
+            background-color:rgb(56 61 88 / 57%);
+            border-radius:4px;
+        }
+        .column hr {
+            background-color:transparent!important;
+        }
+        .footer {
+            background-color:#21232d!important;
+        }
+        .tabs ul {
+            border-color:#383a4c!important;
+        }
+        .tabs.is-boxed li.is-active a {
+            background-color:#364e65!important;
+            color:#fff!important;
+        }
+        .tabs.is-boxed li.is-active a .icon {
+            transform: scale(1.4);
+        }
+        .tabs a {
+            border-bottom:1px solid #383a4c!important;
+            border:none!important;
+        }
+        .tabs a:hover {
+            background-color:#353748!important;
+        }
+        .tabs a .icon {
+            transition: transform .2s ease-in-out;
+            margin-right:6px;
+        }
+        .tabs a:hover .icon {
+            transform: scale(1.4);
+        }
+        input {
+            background-color:#34374c!important;
+            border-color:#34374c!important;
+            color:#eee!important;
+        }
+        input::placeholder {
+            color:#eee;
+        }
+        .button.is-success {
+            background-color:#19843e!important;
+        }
+        .button.is-info {
+            background-color:#155f9e!important;
+        }
+        .button.is-danger {
+            background-color:#b72542!important;
+        }
+        .modal-card-head {
+            background-color:#222633!important;
+            border-color:#425065!important;
+        }
+        .modal-card-head .modal-card-title {
+            color:#eee!important;
+        }
+        .modal-card-body {
+            background-color:#2d3242!important;
+        }
+        .modal-card-foot {
+            background-color:#222633!important;
+            border-color:#425065!important;
+        }
+        .pagination-previous {
+            color:#eee!important;
+        }
+        .pagination-next {
+            color:#eee!important;
+        }
+        .input, .select select, .textarea {
+            background-color:#3c3d4e!important;
+            border-color:#3c3d4e!important;
+            color:#eee!important;
+        }
+        .select:not(.is-multiple):not(.is-loading)::after {
+            border-color:#fff!important;
+        }
+        .message.is-success {
+            background-color:rgb(94 175 123 / 9%)!important;
+        }
+        .message.is-success .message-header {
+            background-color:#2e884d!important;
+        }
+        .message.is-success .box {
+            background-color:rgb(23 249 176 / 8%)!important;
+        }
+        .message.is-success .message-body {
+            color:#37bd65!important;
+        }
+        .pricing-table.is-comparative {
+            background-color:#111!important;
+        }
+        .pricing-table .pricing-plan .plan-header {
+            background-color:#1b1d29!important;
+            color:#eee!important;
+        }
+        .plan-price-amount {
+            color:#fff;
+        }
+        .plan-price {
+            background-color:#262b38!important;
+        }
+        .pricing-table .pricing-plan .plan-price {
+            color:#fff;
+        }
+        .plan-price-currency {
+            color:#30a730!important;
+        }
+        .plan-item {
+            background-color:#1b1e27!important;
+            border-color:#1b1e27!important;
+            color:#fff!important;
+        }
+        .pricing-table .pricing-plan {
+            background-color:#262b38!important;
+            border-color:transparent!important;
+        }
+        .hero {
+            background-color:transparent!important;
+        }
+        .media {
+            border-color:#1b1e27!important;
+        }
+        .button.is-success.is-outlined {
+            background-color: transparent!important;
+        }
+        .content blockquote {
+            background-color: rgb(20 24 33 / 30%)!important;
+            border-left-color: rgb(39 51 68 / 45%)!important;
+        }
+        .message.is-danger .message-header {
+            background-color: #862f40!important;
+        }
+        .message.is-danger {
+            background-color:rgb(132 35 57 / 15%)!important;
+        }
+        .message.is-danger .message-body {
+            color:#f5889e!important;
+        }
+
+        `,
+            head = document.head || document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+
+        head.appendChild(style);
+
+        style.type = 'text/css';
+        if (style.styleSheet){
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+        } else {
+        style.appendChild(document.createTextNode(css));
+        }
+                
+            }
+        });
 
 function httpGetAsync(theUrl, callback)
 {
@@ -140,6 +390,15 @@ function tomatrix() {
     themeName = 'Matrix'
     putSignal()
 }
+function tooutlined() {
+    themeName = 'Outlined'
+    putSignal()
+}
+function toflawless() {
+    themeName = 'Flawless'
+    putSignal()
+}
+//////////////////////////
 
 function createCard(imglink, name_, author_, id_, usedfuncname) {
 
@@ -157,6 +416,7 @@ function createCard(imglink, name_, author_, id_, usedfuncname) {
 
     var image = document.createElement('img')
     image.setAttribute('src', imglink)
+    image.setAttribute('style', 'margin-left: 10%; border-radius: 10px;')
 
     var name = document.createElement('h1')
     name.setAttribute('class', 'title is-6')
@@ -193,9 +453,11 @@ function setThemes() {
 
     var columns = document.createElement('div')
     columns.setAttribute('class', 'columns')
-    columns.appendChild(createCard('https://cdn.discordapp.com/attachments/746375338019258451/763327250963365898/black.png', 'PureBlack', 'Beriff', 'pureblack', 'toblack()'))
+    columns.setAttribute('style', 'margin-top: 9%;')
     columns.appendChild(createCard('https://cdn.discordapp.com/attachments/716672669163978815/763373338503807001/default.png', 'Default', 'Superium', 'default', 'todef()'))
-    columns.appendChild(createCard('https://cdn.discordapp.com/attachments/716672669163978815/763388891944058950/matrix.png', 'Matrix', 'Beriff', 'matrix', 'tomatrix()'))
+    columns.appendChild(createCard('https://cdn.discordapp.com/attachments/746375338019258451/763327250963365898/black.png', 'PureBlack', 'Beriff', 'pureblack', 'toblack()'))
+    columns.appendChild(createCard('https://cdn.discordapp.com/attachments/746638325615689748/763627256215502848/unknown.png', 'Outlined', 'Beriff', 'outlined', 'tooutlined()'))
+    columns.appendChild(createCard('https://cdn.discordapp.com/attachments/763307392821362740/763636787166838784/unknown.png', 'Flawless', 'x_o', 'flawless', 'toflawless()'))
     actualCards.appendChild(columns)
     
     themeTab = document.getElementById('customTab1')
@@ -284,5 +546,13 @@ if (window.location.pathname == "/Shop/") {
     var text_ = document.createTextNode('Item Owners:')
     label.appendChild(text_)
     commentSection.prepend(label)
+
+    //////////CLICKABLE NICKNAMES
+    var media = document.querySelectorAll('.media')
+    for (var i in media.length) {
+        var media_content = media.children[1]
+        var nickname_p = media_content.children[0].children[0]
+        var nickname = media_content.children[0].children[0].children[0]
+    }
 })
 }
